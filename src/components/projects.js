0e3bf911de652fac4c4293e4ddc3225b59f7e382
Jesse Op de Beeck <42997689+jesse-opdebeeck@users.noreply.github.com>
2019-05-24 18:58:05 +0200

    more trials in project rendering to dom

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,57 +5,57 @@ const projects = {
   0: {
     title: "Website Komik Toneel",
     description:
-      "De huidige website van amateur toneelgroep Komik toneel is volledig door mij gemaakt. Begonnen van nul heb ik er een volledig CMS en reservatiesysteem in gestoken.",
-    details: new Array(
+      "De huidige website van amateur toneelgroep Komik Toneel (waar ik zelf ook bij speel) is volledig door mij gemaakt. Begonnen van nul heb ik er een volledig CMS en reservatiesysteem in gestoken.",
+    details: [
       "Design en development",
       "HTML en CSS",
       "PHP (Laravel)",
       "MySQL",
       "Eigen CMS",
       "Reservatiesysteem"
-    ).map(detail => <li>{detail}</li>),
+    ].map(detail => <li>{detail}</li>),
     image: "https://i.imgur.com/9KwQhgm.jpg"
   },
   1: {
     title: "Studentenjob @ Beego",
     description:
       "Gedurende een maand heb ik bij Beego gewerkt, waar men studenten naar mensen thuis stuurt om hen te helpen met hun computerproblemen. Hier heb ik veel bijgeleerd rond zowel hardware als software.",
-    details: new Array(
+    details: [
       "December 2018",
       "Antwerpen en omstreken",
       "Ongeveer 20 mensen geholpen"
-    ).map(detail => <li>{detail}</li>),
+    ].map(detail => <li>{detail}</li>),
     image: "https://i.imgur.com/gOYvWIv.png"
   },
   2: {
     title: "Stage @ HQ",
     description:
       "Bij de start-up HQ heb ik een maand lang stage gelopen en mogen meewerken aan hun website binnen de bedrijfscultuur. Een enorm leerrijke ervaring waarin ik React heb geleerd en heb kunnen genieten van hoe het eraan toe gaat in de werkwereld.",
-    details: new Array(
+    details: [
       "http://hq.app/",
       "Maart 2019",
       "Getuigenissen-slider",
       "React",
       "Javascript"
-    ).map(detail => <li>{detail}</li>),
+    ].map(detail => <li>{detail}</li>),
     image: "https://i.imgur.com/fdmzr9V.png"
   },
   3: {
     title: "Studentenjob @ SDWorx",
     description:
       "Tweemaal werkte ik als student bij SDWorx op de Brouwersvliet in Antwerpen. Het eerste jaar was dit als medewerker op de Facility afdeling, en het tweede jaar bij Corporate HR voor administratieve taken.",
-    details: new Array(
+    details: [
       "Zomer 2017 en 2018",
       "Facility klusjes",
       "Administratie Corporate HR"
-    ).map(detail => <li>{detail}</li>),
+    ].map(detail => <li>{detail}</li>),
     image: "https://i.imgur.com/ZLqgcqJ.png"
   },
   4: {
     title: "Infographic CV",
     description:
       "Een CV is natuurlijk veel interessanter in de vorm van een leuke infographic, en als thema ervoor koos ik de beroemdste fantasy-trilogie in de wereld.",
-    details: new Array("Photoshop & Illustrator", "Volledig in thema").map(
+    details: ["Photoshop & Illustrator", "Volledig in thema"].map(
       detail => "<li>" + { detail } + "</li>"
     ),
     image: "https://i.imgur.com/moqdt77.png"
@@ -64,27 +64,34 @@ const projects = {
     title: "Videopitch",
     description:
       "Welke betere manier om mezelf voor te stellen dan een korte video over mezelf? Het is gemaakt in het formaat van een interview en ik leg er mijn beste capaciteiten in uit.",
-    details: new Array(
+    details: [
       "Adobe Premiere",
       "Zelf opgenomen beelden",
       "Eigen editing",
       "Geweldige muziek"
-    ).map(detail => <li>{detail}</li>),
+    ].map(detail => <li>{detail}</li>),
     image: "https://i.imgur.com/4MHSYsr.jpg"
   },
   6: {
     title: "Helpsessies Dienstencentrum Olijftak",
     description:
       "Tijdens mijn Se-n-Se jaar in Antwerpen hielden we drie verschillende helpdesk-sessies in het dienstencentrum de Olijftak in Borgerhout. Hier hielpen we de senioren met hun vragen over technologie.",
-    details: new Array(
+    details: [
       "Drie sessies",
       "Helpdesk",
       "Presentatie over passwords",
       "Accounts opzetten"
-    ).map(detail => <li>{detail}</li>),
+    ].map(detail => <li>{detail}</li>),
     image: "https://i.imgur.com/dZYC3AX.png"
   }
 };
+
+function ProjectList(props) {
+  const projects = props.projects;
+  const projectItems = projects.map(project => <h1>Test</h1>);
+
+  return <ul>{projectItems}</ul>;
+}
 
 class Projects extends Component {
   state = {
@@ -120,61 +127,10 @@ class Projects extends Component {
             </div>
           </div>
 
-          <h3 className="my-4">Andere projecten</h3>
+          <h3 className="my-4">Alle projecten</h3>
 
-          <div className="row">
-            <div
-              onClick={() => this.setProject()}
-              className="col-md-3 col-sm-6 mb-4"
-            >
-              <a href="javascript:void(0)">
-                <img
-                  className="img-fluid"
-                  src="http://placehold.it/500x300"
-                  alt=""
-                />
-              </a>
-            </div>
-
-            <div
-              onClick={() => this.setProject()}
-              className="col-md-3 col-sm-6 mb-4"
-            >
-              <a href="javascript:void(0)">
-                <img
-                  className="img-fluid"
-                  src="http://placehold.it/500x300"
-                  alt=""
-                />
-              </a>
-            </div>
-
-            <div
-              onClick={() => this.setProject()}
-              className="col-md-3 col-sm-6 mb-4"
-            >
-              <a href="javascript:void(0)">
-                <img
-                  className="img-fluid"
-                  src="http://placehold.it/500x300"
-                  alt=""
-                />
-              </a>
-            </div>
-
-            <div
-              onClick={() => this.setProject()}
-              className="col-md-3 col-sm-6 mb-4"
-            >
-              <a href="javascript:void(0)">
-                <img
-                  className="img-fluid"
-                  src="http://placehold.it/500x300"
-                  alt=""
-                />
-              </a>
-            </div>
-          </div>
+          <div className="row" />
+          <ProjectList projects={projects} />
         </div>
       </React.Fragment>
     );
