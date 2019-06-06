86ad32323b1d33584552224d7799c592cde5427f
Jesse <jesse.opdebeeck@student.scheppersinstituut.be>
2019-06-06 20:54:08 +0200

    render rek image on about page in cols in separate images

--- src/components/over.js
+++ src/components/over.js
@@ -2,14 +2,24 @@ import React, { Component } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
 
+import Himym from "../img/himym.png";
+import Urb from "../img/urb.png";
+import Sky from "../img/sky.png";
+
+import Oscar from "../img/oscar.png";
+import Books from "../img/books.png";
+import Bier from "../img/bier.png";
+
 class Over extends Component {
-  state = {};
+  state = {
+    hovered: "none"
+  };
   render() {
     return (
       <React.Fragment>
         <Header title="Over Jesse" />
         <div className="container">
-          <h1 className="mt-5">Over</h1>
+          <h1 className="mt-5">Maak kennis met me</h1>
           <p className="lead">
             Als je mij beter wil leren kennen is de beste manier natuurlijk om
             met me te praten in een rustig café met een fris biertje in de hand.
@@ -17,10 +27,46 @@ class Over extends Component {
             krijgen van wie ik ben.
           </p>
           <p>
-            Hieronder vind je een foto van een rek in mijn slaapkamer. Ga met je
-            muis over een deel van de afbeelding om te lezen welk verhaal er
-            achter zit.
+            Hier zie je een afbeelding van een rek met een aantal items die veel
+            voor me betekenen en waar een verhaal achter zit. Kijk verder op
+            deze pagina om dat verhaal te weten te komen. Waarschuwing: Jesse
+            staat niet garant dat alle informatie die u zal lezen noodzakelijk
+            noch gewenst zal zijn.
           </p>
+          <div className="row">
+            <img
+              src={Himym}
+              className="col-4 mx-0 px-0 h-100"
+              alt="stuk foto van rek"
+            />
+            <img
+              src={Urb}
+              className="col-4 mx-0 px-0 h-100"
+              alt="stuk foto van rek"
+            />
+            <img
+              src={Sky}
+              className="col-4 mx-0 px-0 h-100"
+              alt="stuk foto van rek"
+            />
+          </div>
+          <div className="row">
+            <img
+              src={Oscar}
+              className="col-3 mx-0 px-0 h-100"
+              alt="stuk foto van rek"
+            />
+            <img
+              src={Books}
+              className="col-6 mx-0 px-0 h-100"
+              alt="stuk foto van rek"
+            />
+            <img
+              src={Bier}
+              className="col-3 mx-0 px-0 h-100"
+              alt="stuk foto van rek"
+            />
+          </div>
         </div>
         <Footer />
       </React.Fragment>
