68942c9e70beeead1f53ce82cd309f84da1ed295
Jesse <jesse.opdebeeck@student.scheppersinstituut.be>
2019-06-06 21:24:19 +0200

    brightness on hover fix, still looking into changing text

--- src/components/over.js
+++ src/components/over.js
@@ -10,10 +10,27 @@ import Oscar from "../img/oscar.png";
 import Books from "../img/books.png";
 import Bier from "../img/bier.png";
 
+const texts = [
+  "Dit zijn de HIMYM boeken",
+  "Dit is de Urbanus CD en Dishonored en Django",
+  "Dit is het Skyrim boek en Zelda CD",
+  "Dit is de Oscar",
+  "Dit zijn de boeken",
+  "Hier staan de bierflesjes"
+];
+
 class Over extends Component {
   state = {
-    hovered: "none"
+    text: ""
   };
+
+  changeText = i => {
+    var index = i;
+    if (this.state.text !== texts[index]) {
+      this.setState({ text: texts[index] });
+    }
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -27,44 +44,51 @@ class Over extends Component {
             krijgen van wie ik ben.
           </p>
           <p>
-            Hier zie je een afbeelding van een rek met een aantal items die veel
-            voor me betekenen en waar een verhaal achter zit. Kijk verder op
-            deze pagina om dat verhaal te weten te komen. Waarschuwing: Jesse
-            staat niet garant dat alle informatie die u zal lezen noodzakelijk
-            noch gewenst zal zijn.
+            Hier zie je een afbeelding van een rek met een aantal items die een
+            betekenis voor me hebben en waar een verhaal achter zit. Klik op een
+            sectie van het rek om dat verhaal te weten te komen. Waarschuwing:
+            Jesse staat niet garant dat alle informatie die u zal lezen
+            noodzakelijk noch gewenst zal zijn.
           </p>
-          <div className="row">
+          <p className="col-8 offset-2 my-5 rek-text">{this.state.text}</p>
+          <div className="row mt-5">
             <img
               src={Himym}
-              className="col-4 mx-0 px-0 h-100"
+              className="col-4 mx-0 px-0 h-100 rek-part"
               alt="stuk foto van rek"
+              onClick={this.changeText(0)}
             />
             <img
               src={Urb}
-              className="col-4 mx-0 px-0 h-100"
+              className="col-4 mx-0 px-0 h-100 rek-part light"
               alt="stuk foto van rek"
+              onClick={this.changeText}
             />
             <img
               src={Sky}
-              className="col-4 mx-0 px-0 h-100"
+              className="col-4 mx-0 px-0 h-100 rek-part"
               alt="stuk foto van rek"
+              onClick={this.changeText}
             />
           </div>
           <div className="row">
             <img
               src={Oscar}
-              className="col-3 mx-0 px-0 h-100"
+              className="col-3 mx-0 px-0 h-100 rek-part"
               alt="stuk foto van rek"
+              onClick={this.changeText}
             />
             <img
               src={Books}
-              className="col-6 mx-0 px-0 h-100"
+              className="col-6 mx-0 px-0 h-100 rek-part"
               alt="stuk foto van rek"
+              onClick={this.changeText}
             />
             <img
               src={Bier}
-              className="col-3 mx-0 px-0 h-100"
+              className="col-3 mx-0 px-0 h-100 rek-part"
               alt="stuk foto van rek"
+              onClick={this.changeText}
             />
           </div>
         </div>
