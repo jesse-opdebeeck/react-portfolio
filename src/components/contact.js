a238b84c7976a22b33337eabae839c909990484b
Jesse <jesse.opdebeeck@student.scheppersinstituut.be>
2019-06-10 13:24:32 +0200

    edited contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
+
 import { SocialIcon } from "react-social-icons";
 import callmeup from "../img/callmeup.jpg";
 
@@ -10,64 +11,49 @@ class Contact extends Component {
     return (
       <React.Fragment>
         <Header title="Contact" />
-        <div class="container">
-          <div class="row align-items-center my-5">
-            <div class="col-lg-7">
+        <div className="container">
+          <div className="row align-items-center my-5">
+            <div className="col-lg-7">
               <img
-                class="img-fluid rounded mb-4 mb-lg-0 callmeup"
+                className="img-fluid rounded mb-4 mb-lg-0 callmeup"
                 src={callmeup}
                 alt="Bel me op!"
               />
             </div>
-            <div class="col-lg-5">
-              <h1 class="font-weight-light">Contacteer mij</h1>
+            <div className="col-lg-5">
+              <h1 className="font-weight-light">Contacteer mij</h1>
               <p>
                 Genoeg gezien? Uiteraard staat u te popelen om met mij in
                 contact te komen! Klik op deze knop om uw e-mailprogramma te
                 openen, of bekijk onderaan de andere opties.
               </p>
-              <a class="btn btn-primary" href="mailto:jesseodb@hotmail.com">
+              <a className="btn btn-primary" href="mailto:jesseodb@hotmail.com">
                 Open mijn e-mail!
               </a>
             </div>
           </div>
-          <div class="row">
-            <div class="col-md-6 mb-5">
-              <div class="card h-100 contact-card text-center">
-                <div class="card-body">
-                  <h2 class="card-title">Direct bericht</h2>
-                  <p class="card-text">Geef hier een snel berichtje in!</p>
-                  <form className="contact-dm-form">
-                    <textarea
-                      className="form-control"
-                      rows="3"
-                      placeholder="Uw bericht"
-                    />
-                    <br />
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Uw e-mailadres of telefoonnummer"
-                    />
-                    <small class="form-text text-muted">
-                      Hoe kan ik u beantwoorden?
-                    </small>
-                    <br />
-                    <button
-                      href="#"
-                      class="btn btn-primary btn-md"
-                      type="submit"
-                    >
-                      Verstuur
-                    </button>
-                  </form>
+          <div className="row">
+            <div className="col-md-6 mb-5">
+              <div className="card h-100 contact-card text-center">
+                <div className="card-body">
+                  <h2 className="card-title">Contactgegevens</h2>
+                  <p className="card-text">Hoe kan je mij best bereiken?</p>
+                  <ul className="list-unstyled">
+                    <li className="mt-2 my-2">
+                      <b>WhatsApp: </b>+32 (0) 471 39 15 88
+                    </li>
+                    <li className="mt-2">
+                      <b>E-mail: </b>jesseodb@hotmail.com
+                    </li>
+                  </ul>
+                  <p>Of stuur me een snel berichtje op Messenger!</p>
                 </div>
               </div>
             </div>
-            <div class="col-md-6 mb-5">
-              <div class="card contact-card">
-                <div class="card-body text-center">
-                  <h2 class="card-title mb-3">Social media</h2>
+            <div className="col-md-6 mb-5">
+              <div className="card contact-card">
+                <div className="card-body text-center">
+                  <h2 className="card-title mb-3">Social media</h2>
                   <SocialIcon
                     url="http://www.facebook.com/jesseodb"
                     target="_blank"
